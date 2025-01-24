ee380010e5e8313ad1bff6c64fdda88bda5a6ac8
hidawns <134594505+hidawns@users.noreply.github.com>
2025-01-24 17:53:42 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import seaborn as sns
 day_df = pd.read_csv('processed_day.csv')
 
 # Set up Streamlit page
-st.title('Dashboard Analisis Penyewaan Sepeda')
+st.title('Dashboard Permintaan Penyewaan Sepeda')
 
 # Pengaruh Cuaca terhadap Permintaan Penyewaan Sepeda
 st.subheader('Pengaruh Cuaca terhadap Permintaan Penyewaan Sepeda')
@@ -19,16 +19,17 @@ weather_avg_sorted = weather_avg.sort_values(ascending=False)
 fig, ax = plt.subplots(figsize=(10, 6))
 bar_colors = sns.color_palette('Blues_d', len(weather_avg_sorted))
 ax.bar(weather_labels, weather_avg_sorted, color=bar_colors)
-ax.set_title('Pengaruh Cuaca terhadap Permintaan Penyewaan Sepeda')
-ax.set_xlabel('Kondisi Cuaca')
-ax.set_ylabel('Rata-Rata Penyewaan Sepeda')
+#ax.set_title('Pengaruh Cuaca terhadap Permintaan Penyewaan Sepeda')
+ax.set_xlabel('Cuaca')
+ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
 st.pyplot(fig)
 
 weather_table = pd.DataFrame({
-    'Kondisi Cuaca': weather_labels,
-    'Rata-Rata Penyewaan Sepeda': weather_avg_sorted.values
+    'Cuaca': weather_labels,
+    'Rata-Rata Jml Penyewaan Sepeda': weather_avg_sorted.values
 })
+weather_table['Rata-Rata Jml Penyewaan Sepeda'] = weather_table['Rata-Rata Jml Penyewaan Sepeda'].round(0)
 st.write(weather_table)
 
 # Pengaruh Musim terhadap Permintaan Penyewaan Sepeda
@@ -41,19 +42,20 @@ season_avg_sorted = season_avg.sort_values(ascending=False)
 fig, ax = plt.subplots(figsize=(10, 6))
 bar_colors = sns.color_palette('Blues_d', len(season_avg_sorted))
 ax.bar(season_labels, season_avg_sorted, color=bar_colors)
-ax.set_title('Pengaruh Musim terhadap Permintaan Penyewaan Sepeda')
+#ax.set_title('Pengaruh Musim terhadap Permintaan Penyewaan Sepeda')
 ax.set_xlabel('Musim')
-ax.set_ylabel('Rata-Rata Penyewaan Sepeda')
+ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
 st.pyplot(fig)
 
 season_table = pd.DataFrame({
     'Musim': season_labels,
-    'Rata-Rata Penyewaan Sepeda': season_avg_sorted.values})
+    'Rata-Rata Jml Penyewaan Sepeda': season_avg_sorted.values})
+season_table['Rata-Rata Jml Penyewaan Sepeda'] = season_table['Rata-Rata Jml Penyewaan Sepeda'].round(0)
 st.write(season_table)
 
-# Perbedaan Pola Penyewaan antara Hari Kerja dan Akhir Pekan
-st.subheader('Perbedaan Pola Penyewaan: Hari Kerja vs Akhir Pekan')
+# Perbedaan Permintaan Penyewaan Sepeda berdasarkan Working Day
+st.subheader('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Working Day')
 
 workingday_labels = ['Akhir Pekan(0)', 'Hari Kerja (1)']
 workingday_avg = day_df.groupby('workingday')['cnt'].mean().reindex([0, 1], fill_value=0)
@@ -61,20 +63,21 @@ workingday_avg = day_df.groupby('workingday')['cnt'].mean().reindex([0, 1], fill
 fig, ax = plt.subplots(figsize=(8, 6))
 bar_colors = sns.color_palette('Blues_d', len(workingday_avg))
 ax.bar(workingday_labels, workingday_avg, color=bar_colors)
-ax.set_title('Rata-rata Peminjaman Sepeda: Hari Kerja vs Akhir Pekan')
-ax.set_xlabel('Kategori Hari')
-ax.set_ylabel('Rata-Rata Penyewaan Sepeda')
+#ax.set_title('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Working Day')
+ax.set_xlabel('Working Day')
+ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
 st.pyplot(fig)
 
 workingday_table = pd.DataFrame({
-    'Kategori Hari': workingday_labels,
-    'Rata-Rata Penyewaan Sepeda': workingday_avg.values
+    'Working Day': workingday_labels,
+    'Rata-Rata Jml Penyewaan Sepeda': workingday_avg.values
 })
+workingday_table['Rata-Rata Jml Penyewaan Sepeda'] = workingday_table['Rata-Rata Jml Penyewaan Sepeda'].round(0)
 st.write(workingday_table)
 
-# Perbedaan Permintaan antara Pengguna Kasual dan Terdaftar
-st.subheader('Perbedaan Permintaan Penyewaan: Kasual vs Terdaftar')
+# Perbedaan Permintaan Penyewaan Sepeda berdasarkan Jenis Keanggotaan
+st.subheader('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Jenis Keanggotaan')
 
 user_labels = ['Casual', 'Registered']
 user_avg = day_df[['casual', 'registered']].mean()
@@ -82,13 +85,14 @@ user_avg = day_df[['casual', 'registered']].mean()
 fig, ax = plt.subplots(figsize=(8, 6))
 bar_colors = sns.color_palette('Blues_d', len(user_avg))
 ax.bar(user_labels, user_avg, color=bar_colors)
-ax.set_title('Rata-rata Peminjaman Sepeda: Casual vs Registered')
-ax.set_xlabel('Kategori Pengguna')
-ax.set_ylabel('Rata-Rata Penyewaan Sepeda')
+#ax.set_title('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Jenis Keanggotaan')
+ax.set_xlabel('Jenis Keanggotaan')
+ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
 st.pyplot(fig)
 
 user_table = pd.DataFrame({
-    'Kategori Pengguna': user_labels,
-    'Rata-Rata Penyewaan Sepeda': user_avg.values })
+    'Jenis Keanggotaan': user_labels,
+    'Rata-Rata Jml Penyewaan Sepeda': user_avg.values })
+user_table['Rata-Rata Jml Penyewaan Sepeda'] = user_table['Rata-Rata Jml Penyewaan Sepeda'].round(0)
 st.write(user_table)
