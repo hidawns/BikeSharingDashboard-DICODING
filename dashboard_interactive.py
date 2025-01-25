4c96966796bfbe0df88094c92c337828c8707817
hidawns <134594505+hidawns@users.noreply.github.com>
2025-01-25 18:09:26 +0700

    Update dashboard_interactive.py

--- dashboard_interactive.py
+++ dashboard_interactive.py
@@ -6,14 +6,13 @@ import seaborn as sns
 day_df = pd.read_csv('processed_day.csv')
 day_df['dteday'] = pd.to_datetime(day_df['dteday'])
 
-st.sidebar.image("bicycle.png", use_container_width=True)
+st.sidebar.image("bicycle.png", use_column_width=True)
 st.sidebar.header("Filter Data")
 
 min_date = day_df['dteday'].min()
 max_date = day_df['dteday'].max()
-
-start_date = st.sidebar.date_input("Tanggal Mulai", min_date, min_value=min_date, max_value=max_date)
-end_date = st.sidebar.date_input("Tanggal Akhir", max_date, min_value=min_date, max_value=max_date)
+start_date = st.sidebar.date_input("Mulai Tanggal", min_date, min_value=min_date, max_value=max_date)
+end_date = st.sidebar.date_input("Akhir Tanggal", max_date, min_value=min_date, max_value=max_date)
 
 if start_date > end_date:
     st.sidebar.error("Tanggal mulai harus lebih kecil atau sama dengan tanggal akhir.")
@@ -69,12 +68,11 @@ st.write(season_table)
 st.subheader('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Working Day')
 
 workingday_labels = ['Akhir Pekan(0)', 'Hari Kerja (1)']
-workingday_avg = day_df.groupby('workingday')['cnt'].mean().reindex([0, 1], fill_value=0)
+workingday_avg = filtered_df.groupby('workingday')['cnt'].mean().reindex([0, 1], fill_value=0)
 
 fig, ax = plt.subplots(figsize=(8, 6))
 bar_colors = sns.color_palette('Blues_d', len(workingday_avg))
 ax.bar(workingday_labels, workingday_avg, color=bar_colors)
-#ax.set_title('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Working Day')
 ax.set_xlabel('Working Day')
 ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
@@ -91,12 +89,11 @@ st.write(workingday_table)
 st.subheader('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Jenis Keanggotaan')
 
 user_labels = ['Casual', 'Registered']
-user_avg = day_df[['casual', 'registered']].mean()
+user_avg = filtered_df[['casual', 'registered']].mean()
 
 fig, ax = plt.subplots(figsize=(8, 6))
 bar_colors = sns.color_palette('Blues_d', len(user_avg))
 ax.bar(user_labels, user_avg, color=bar_colors)
-#ax.set_title('Perbedaan Permintaan Penyewaan Sepeda berdasarkan Jenis Keanggotaan')
 ax.set_xlabel('Jenis Keanggotaan')
 ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
@@ -114,13 +111,12 @@ st.subheader('Rata-Rata Penyewaan Sepeda berdasarkan Kategori Windspeed')
 windspeed_bins = [0, 0.1, 0.3, 0.5, 1]  # Rentang kecepatan angin (windspeed)
 windspeed_labels = ['Low', 'Medium', 'High', 'Very High']
 
-day_df['windspeed_category'] = pd.cut(day_df['windspeed'], bins=windspeed_bins, labels=windspeed_labels)
-average_cnt_by_windspeed = day_df.groupby('windspeed_category', observed=False)['cnt'].mean()
+filtered_df['windspeed_category'] = pd.cut(filtered_df['windspeed'], bins=windspeed_bins, labels=windspeed_labels)
+average_cnt_by_windspeed = filtered_df.groupby('windspeed_category', observed=False)['cnt'].mean()
 
 fig, ax = plt.subplots(figsize=(6, 5))
 colors = sns.color_palette("Greys", len(average_cnt_by_windspeed))
 sns.barplot(x=average_cnt_by_windspeed.index, y=average_cnt_by_windspeed.values, palette=colors, hue=average_cnt_by_windspeed.index, legend=False)
-#ax.set_title('Rata-Rata Jumlah Penyewaan Sepeda berdasarkan Kategori Windspeed')
 ax.set_xlabel('Kategori Windspeed')
 ax.set_ylabel('Rata Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
@@ -139,13 +135,12 @@ st.subheader('Rata-Rata Penyewaan Sepeda berdasarkan Kategori Humidity')
 hum_bins = [0, 0.4, 0.7, 1]  # Rentang kelembapan (hum)
 hum_labels = ['Low', 'Medium', 'High']
 
-day_df['hum_category'] = pd.cut(day_df['hum'], bins=hum_bins, labels=hum_labels)
-average_cnt_by_hum = day_df.groupby('hum_category', observed=False)['cnt'].mean()
+filtered_df['hum_category'] = pd.cut(filtered_df['hum'], bins=hum_bins, labels=hum_labels)
+average_cnt_by_hum = filtered_df.groupby('hum_category', observed=False)['cnt'].mean()
 
 fig, ax = plt.subplots(figsize=(6, 5))
 colors = sns.color_palette("Greens", len(average_cnt_by_hum))
 sns.barplot(x=average_cnt_by_hum.index, y=average_cnt_by_hum.values, palette=colors, hue=average_cnt_by_hum.index, legend=False)
-#ax.set_title('Rata-Rata Jumlah Penyewaan Sepeda berdasarkan Kategori Humidity')
 ax.set_xlabel('Kategori Humidity')
 ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
@@ -164,13 +159,12 @@ st.subheader('Rata-Rata Penyewaan Sepeda berdasarkan Kategori Temperature')
 temp_bins = [0, 0.3, 0.6, 0.8, 1]  # Rentang suhu (temp)
 temp_labels = ['Low', 'Medium', 'High', 'Very High']
 
-day_df['temp_category'] = pd.cut(day_df['temp'], bins=temp_bins, labels=temp_labels)
-average_cnt_by_temp = day_df.groupby('temp_category', observed=False)['cnt'].mean()
+filtered_df['temp_category'] = pd.cut(filtered_df['temp'], bins=temp_bins, labels=temp_labels)
+average_cnt_by_temp = filtered_df.groupby('temp_category', observed=False)['cnt'].mean()
 
 fig, ax = plt.subplots(figsize=(6, 5))
 colors = sns.color_palette("Oranges", len(average_cnt_by_temp))
 sns.barplot(x=average_cnt_by_temp.index, y=average_cnt_by_temp.values, palette=colors, hue=average_cnt_by_temp.index, legend=False)
-#ax.set_title('Rata-Rata Jumlah Penyewaan Sepeda berdasarkan Kategori Temperature')
 ax.set_xlabel('Kategori Temperature')
 ax.set_ylabel('Rata-Rata Jml Penyewaan Sepeda')
 plt.tight_layout()
